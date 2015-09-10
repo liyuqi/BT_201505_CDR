dd9011f51f6add324033f002d13b20682f28a69e
liyuqi <am997liyuqi@gmail.com>
2015-09-10 14:52:16 +0800

    distinct

--- mongo_prepare_data/cep3g_stat_phone.js
+++ mongo_prepare_data/cep3g_stat_phone.js
@@ -28,17 +28,17 @@ var agg_3g = db.cep3g_join.aggregate([
             , HO : 1
             , HO_SECOND : 1
 
-            , SUM_CALLED_COUNT_0_3  :{$cond :[{$and:[{$gte:["$HO_SECOND",0 ]},{$lte:["$HO_SECOND",3 ]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_3_5  :{$cond :[{$and:[{$gt :["$HO_SECOND",3 ]},{$lte:["$HO_SECOND",5 ]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_5_7  :{$cond :[{$and:[{$gt :["$HO_SECOND",5 ]},{$lte:["$HO_SECOND",7 ]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_7_10 :{$cond :[{$and:[{$gt :["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_10UP :{$cond :[       {$gt :["$HO_SECOND",10]}                            ,"$HO",0]}
-
-            , SUM_CALLED_SECOND_0_3  :{$cond :[{$and:[{$gte:["$HO_SECOND",0 ]},{$lte:["$HO_SECOND",3 ]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_3_5  :{$cond :[{$and:[{$gt :["$HO_SECOND",3 ]},{$lte:["$HO_SECOND",5 ]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_5_7  :{$cond :[{$and:[{$gt :["$HO_SECOND",5 ]},{$lte:["$HO_SECOND",7 ]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_7_10 :{$cond :[{$and:[{$gt :["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_10UP :{$cond :[       {$gt :["$HO_SECOND",10]}                            ,"$HO_SECOND",0]}
+            //, SUM_CALLED_COUNT_0_3  :{$cond :[{$and:[{$gte:["$HO_SECOND",0 ]},{$lte:["$HO_SECOND",3 ]} ]},"$HO",0]}
+            //, SUM_CALLED_COUNT_3_5  :{$cond :[{$and:[{$gt :["$HO_SECOND",3 ]},{$lte:["$HO_SECOND",5 ]} ]},"$HO",0]}
+            //, SUM_CALLED_COUNT_5_7  :{$cond :[{$and:[{$gt :["$HO_SECOND",5 ]},{$lte:["$HO_SECOND",7 ]} ]},"$HO",0]}
+            //, SUM_CALLED_COUNT_7_10 :{$cond :[{$and:[{$gt :["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO",0]}
+            //, SUM_CALLED_COUNT_10UP :{$cond :[       {$gt :["$HO_SECOND",10]}                            ,"$HO",0]}
+            //
+            //, SUM_CALLED_SECOND_0_3  :{$cond :[{$and:[{$gte:["$HO_SECOND",0 ]},{$lte:["$HO_SECOND",3 ]} ]},"$HO_SECOND",0]}
+            //, SUM_CALLED_SECOND_3_5  :{$cond :[{$and:[{$gt :["$HO_SECOND",3 ]},{$lte:["$HO_SECOND",5 ]} ]},"$HO_SECOND",0]}
+            //, SUM_CALLED_SECOND_5_7  :{$cond :[{$and:[{$gt :["$HO_SECOND",5 ]},{$lte:["$HO_SECOND",7 ]} ]},"$HO_SECOND",0]}
+            //, SUM_CALLED_SECOND_7_10 :{$cond :[{$and:[{$gt :["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO_SECOND",0]}
+            //, SUM_CALLED_SECOND_10UP :{$cond :[       {$gt :["$HO_SECOND",10]}                            ,"$HO_SECOND",0]}
 
             //, record_type :1
             //, DISTINCT:{$cond : [
@@ -77,20 +77,20 @@ var agg_3g = db.cep3g_join.aggregate([
             , HO_CALLED_COUNT:{$sum:"$HO"}
             , HO_CALLED_SECOND:{$sum:"$HO_SECOND"}
 
-            , SUM_CALLED_COUNT_0_3 : {$sum:"$SUM_CALLED_COUNT_0_3"}
-            , SUM_CALLED_COUNT_3_5 : {$sum:"$SUM_CALLED_COUNT_0_3"}
-            , SUM_CALLED_COUNT_5_7 : {$sum:"$SUM_CALLED_COUNT_3_5"}
-            , SUM_CALLED_COUNT_7_10: {$sum:"$SUM_CALLED_COUNT_5_7"}
-            , SUM_CALLED_COUNT_10UP: {$sum:"$SUM_CALLED_COUNT_7_10"}
-
-            , SUM_CALLED_SECOND_0_3 : {$sum:"$SUM_CALLED_SECOND_0_3"}
-            , SUM_CALLED_SECOND_3_5 : {$sum:"$SUM_CALLED_SECOND_0_3"}
-            , SUM_CALLED_SECOND_5_7 : {$sum:"$SUM_CALLED_SECOND_3_5"}
-            , SUM_CALLED_SECOND_7_10: {$sum:"$SUM_CALLED_SECOND_5_7"}
-            , SUM_CALLED_SECOND_10UP: {$sum:"$SUM_CALLED_SECOND_7_10"}
+            //, SUM_CALLED_COUNT_0_3 : {$sum:"$SUM_CALLED_COUNT_0_3"}
+            //, SUM_CALLED_COUNT_3_5 : {$sum:"$SUM_CALLED_COUNT_0_3"}
+            //, SUM_CALLED_COUNT_5_7 : {$sum:"$SUM_CALLED_COUNT_3_5"}
+            //, SUM_CALLED_COUNT_7_10: {$sum:"$SUM_CALLED_COUNT_5_7"}
+            //, SUM_CALLED_COUNT_10UP: {$sum:"$SUM_CALLED_COUNT_7_10"}
+            //
+            //, SUM_CALLED_SECOND_0_3 : {$sum:"$SUM_CALLED_SECOND_0_3"}
+            //, SUM_CALLED_SECOND_3_5 : {$sum:"$SUM_CALLED_SECOND_0_3"}
+            //, SUM_CALLED_SECOND_5_7 : {$sum:"$SUM_CALLED_SECOND_3_5"}
+            //, SUM_CALLED_SECOND_7_10: {$sum:"$SUM_CALLED_SECOND_5_7"}
+            //, SUM_CALLED_SECOND_10UP: {$sum:"$SUM_CALLED_SECOND_7_10"}
 
             //, DISTINCT:{$addToSet:"$DISTINCT"}
-            , HO_DISTINCT:{$addToSet:"$DISTINCT"}
+            , HO_DISTINCT:{$addToSet:"$HO_DISTINCT"}
         }}
         ,{$project:{
             _id:0
@@ -116,17 +116,17 @@ var agg_3g = db.cep3g_join.aggregate([
             , HO_CALLED_SECOND :1
             , HO_CALLED_MINUTES :{$divide:["$HO_CALLED_SECOND",60]}
 
-            , SUM_CALLED_COUNT_0_3 : 1
-            , SUM_CALLED_COUNT_3_5 : 1
-            , SUM_CALLED_COUNT_5_7 : 1
-            , SUM_CALLED_COUNT_7_10: 1
-            , SUM_CALLED_COUNT_10UP: 1
-
-            , SUM_CALLED_MINUTES_0_3 : {$divide:["$SUM_CALLED_SECOND_0_3" ,60]}
-            , SUM_CALLED_MINUTES_3_5 : {$divide:["$SUM_CALLED_SECOND_3_5" ,60]}
-            , SUM_CALLED_MINUTES_5_7 : {$divide:["$SUM_CALLED_SECOND_5_7" ,60]}
-            , SUM_CALLED_MINUTES_7_10: {$divide:["$SUM_CALLED_SECOND_7_10",60]}
-            , SUM_CALLED_MINUTES_10UP: {$divide:["$SUM_CALLED_SECOND_10UP",60]}
+            //, SUM_CALLED_COUNT_0_3 : 1
+            //, SUM_CALLED_COUNT_3_5 : 1
+            //, SUM_CALLED_COUNT_5_7 : 1
+            //, SUM_CALLED_COUNT_7_10: 1
+            //, SUM_CALLED_COUNT_10UP: 1
+            //
+            //, SUM_CALLED_MINUTES_0_3 : {$divide:["$SUM_CALLED_SECOND_0_3" ,60]}
+            //, SUM_CALLED_MINUTES_3_5 : {$divide:["$SUM_CALLED_SECOND_3_5" ,60]}
+            //, SUM_CALLED_MINUTES_5_7 : {$divide:["$SUM_CALLED_SECOND_5_7" ,60]}
+            //, SUM_CALLED_MINUTES_7_10: {$divide:["$SUM_CALLED_SECOND_7_10",60]}
+            //, SUM_CALLED_MINUTES_10UP: {$divide:["$SUM_CALLED_SECOND_10UP",60]}
 
             //, DISTINCT:1
             , HO_DISTINCT:1
