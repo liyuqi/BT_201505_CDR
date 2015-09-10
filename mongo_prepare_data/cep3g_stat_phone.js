185471c3258a556705bd271a9739219d883c481b
liyuqi <am997liyuqi@gmail.com>
2015-09-10 12:04:39 +0800

    distinct record type

--- mongo_prepare_data/cep3g_stat_phone.js
+++ mongo_prepare_data/cep3g_stat_phone.js
@@ -40,15 +40,15 @@ var agg_3g = db.cep3g_join.aggregate([
             , SUM_CALLED_SECOND_7_10 :{$cond :[{$and:[{$gt:["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO_SECOND",0]}
             , SUM_CALLED_SECOND_10UP :{$cond :[       {$gt:["$HO_SECOND",10]}                            ,"$HO_SECOND",0]}
 
-            , record_type :1
-            , DISTINCT:
+            //, record_type :1
+            //, DISTINCT:{$cond : [
+            //    {"$record_type":{$in:["1"]}}
+            //    //{$eg:["$record_type","1"]}
+            //    , "$called_number"
+            //    , ""
+            //]}
 
-            {$cond : [
-                //{"$record_type":{$:["1"]}}
-                {$eg:["$record_type","1"]}
-                , "$called_number"
-                , ""
-            ]}
+            , DISTINCT:"$called_number"
         }}
         ,{$group:{
             _id: {
@@ -87,7 +87,7 @@ var agg_3g = db.cep3g_join.aggregate([
             , SUM_CALLED_SECOND_7_10: {$sum:"$SUM_CALLED_SECOND_5_7"}
             , SUM_CALLED_SECOND_10UP: {$sum:"$SUM_CALLED_SECOND_7_10"}
 
-            , DISTINCT:{$addToSet:"$DISTINCT"}
+            //, DISTINCT:{$addToSet:"$DISTINCT"}
         }}
         ,{$project:{
             _id:0
@@ -125,7 +125,7 @@ var agg_3g = db.cep3g_join.aggregate([
             , SUM_CALLED_MINUTES_7_10: {$divide:["$SUM_CALLED_SECOND_7_10",60]}
             , SUM_CALLED_MINUTES_10UP: {$divide:["$SUM_CALLED_SECOND_10UP",60]}
 
-            , DISTINCT:1
+            //, DISTINCT:1
         }}
         ,{    $out:"cep3g_stat_phone"}
     ]
