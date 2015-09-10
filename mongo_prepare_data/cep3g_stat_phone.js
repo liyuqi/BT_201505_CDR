0e0ae5c60a9ce5ec7e817589ea5d44ad6be42db9
liyuqi <am997liyuqi@gmail.com>
2015-09-10 11:44:45 +0800

    distinct

--- mongo_prepare_data/cep3g_stat_phone.js
+++ mongo_prepare_data/cep3g_stat_phone.js
@@ -43,10 +43,10 @@ var agg_3g = db.cep3g_join.aggregate([
             , DISTINCT:
 
             {$cond : [
-                {record_type:{$in:["1"]}},
+                {"$record_type":{$in:["1"]}},
                 "$called_number"
                 , {$cond : [
-                    {record_type:{$in:["2"]}}
+                    {"$record_type":{$in:["2"]}}
                     ,"$calling_imsi"
                     ,""
                 ]}
