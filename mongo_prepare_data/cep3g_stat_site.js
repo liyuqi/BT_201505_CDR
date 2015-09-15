7634e8542ee856f60c86c92ab2ed800b38a9d5bf
liyuqi <am997liyuqi@gmail.com>
2015-09-15 12:08:24 +0800

    distinct

--- mongo_prepare_data/cep3g_stat_site.js
+++ mongo_prepare_data/cep3g_stat_site.js
@@ -5,10 +5,10 @@ var agg_3g = db.cep3g_join.aggregate([
         {$match: {
             /*time: interval,up_falg:1,*/
             record_type:{$in:["1","2"]}
-            
+
         }}
         ,{$project:{
-            DATE:{ $substr: [ "$date_time", 0, 10 ] }
+              DATE:{ $substr: [ "$date_time", 0, 10 ] }
             , HOUR:{ $substr: [ "$date_time", 11, 2 ] }
             , NETWORK_TYPE : 1
             , END_CODE : "$cause_for_termination"
@@ -23,25 +23,31 @@ var agg_3g = db.cep3g_join.aggregate([
             //, VENDOR : "$VENDOR"
             //, MODEL  : "$MODEL"
 
-            , HO_DISTINCT:{$cond :[{$gt:["$HO",0]},"$called_number",""]}
-            , HO : 1
-            , HO_SECOND : 1
+            //, HO_DISTINCT:{$cond :[{$gt:["$HO",0]},"$CALL_NUMBER",""]}
+            //, HO : 1
+            //, HO_SECOND : 1
 
-            , SUM_CALLED_COUNT_0_3  :{$cond :[{$and:[{$gte:["$HO_SECOND",0 ]},{$lte:["$HO_SECOND",3 ]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_3_5  :{$cond :[{$and:[{$gt :["$HO_SECOND",3 ]},{$lte:["$HO_SECOND",5 ]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_5_7  :{$cond :[{$and:[{$gt :["$HO_SECOND",5 ]},{$lte:["$HO_SECOND",7 ]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_7_10 :{$cond :[{$and:[{$gt :["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO",0]}
-            , SUM_CALLED_COUNT_10UP :{$cond :[       {$gt :["$HO_SECOND",10]}                            ,"$HO",0]}
+            , DISTINCT_CALLED_COUNT_0_3  :{$cond :[{$and:[{$gte:["$CALL_DURATION",0 ]},{$lte:["$CALL_DURATION",3 ]} ]},"$CALL_NUMBER",0]}
+            , DISTINCT_CALLED_COUNT_3_5  :{$cond :[{$and:[{$gt :["$CALL_DURATION",3 ]},{$lte:["$CALL_DURATION",5 ]} ]},"$CALL_NUMBER",0]}
+            , DISTINCT_CALLED_COUNT_5_7  :{$cond :[{$and:[{$gt :["$CALL_DURATION",5 ]},{$lte:["$CALL_DURATION",7 ]} ]},"$CALL_NUMBER",0]}
+            , DISTINCT_CALLED_COUNT_7_10 :{$cond :[{$and:[{$gt :["$CALL_DURATION",7 ]},{$lte:["$CALL_DURATION",10]} ]},"$CALL_NUMBER",0]}
+            , DISTINCT_CALLED_COUNT_10UP :{$cond :[       {$gt :["$CALL_DURATION",10]}                                ,"$CALL_NUMBER",0]}
 
-            , SUM_CALLED_SECOND_0_3  :{$cond :[{$and:[{$gte:["$HO_SECOND",0 ]},{$lte:["$HO_SECOND",3 ]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_3_5  :{$cond :[{$and:[{$gt :["$HO_SECOND",3 ]},{$lte:["$HO_SECOND",5 ]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_5_7  :{$cond :[{$and:[{$gt :["$HO_SECOND",5 ]},{$lte:["$HO_SECOND",7 ]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_7_10 :{$cond :[{$and:[{$gt :["$HO_SECOND",7 ]},{$lte:["$HO_SECOND",10]} ]},"$HO_SECOND",0]}
-            , SUM_CALLED_SECOND_10UP :{$cond :[       {$gt :["$HO_SECOND",10]}                            ,"$HO_SECOND",0]}
+            , SUM_CALLED_COUNT_0_3  :{$cond :[{$and:[{$gte:["$CALL_DURATION",0 ]},{$lte:["$CALL_DURATION",3 ]} ]},1,0]}
+            , SUM_CALLED_COUNT_3_5  :{$cond :[{$and:[{$gt :["$CALL_DURATION",3 ]},{$lte:["$CALL_DURATION",5 ]} ]},1,0]}
+            , SUM_CALLED_COUNT_5_7  :{$cond :[{$and:[{$gt :["$CALL_DURATION",5 ]},{$lte:["$CALL_DURATION",7 ]} ]},1,0]}
+            , SUM_CALLED_COUNT_7_10 :{$cond :[{$and:[{$gt :["$CALL_DURATION",7 ]},{$lte:["$CALL_DURATION",10]} ]},1,0]}
+            , SUM_CALLED_COUNT_10UP :{$cond :[       {$gt :["$CALL_DURATION",10]}                                ,1,0]}
+
+            , SUM_CALLED_SECOND_0_3  :{$cond :[{$and:[{$gte:["$CALL_DURATION",0 ]},{$lte:["$CALL_DURATION",3 ]} ]},"$CALL_DURATION",0]}
+            , SUM_CALLED_SECOND_3_5  :{$cond :[{$and:[{$gt :["$CALL_DURATION",3 ]},{$lte:["$CALL_DURATION",5 ]} ]},"$CALL_DURATION",0]}
+            , SUM_CALLED_SECOND_5_7  :{$cond :[{$and:[{$gt :["$CALL_DURATION",5 ]},{$lte:["$CALL_DURATION",7 ]} ]},"$CALL_DURATION",0]}
+            , SUM_CALLED_SECOND_7_10 :{$cond :[{$and:[{$gt :["$CALL_DURATION",7 ]},{$lte:["$CALL_DURATION",10]} ]},"$CALL_DURATION",0]}
+            , SUM_CALLED_SECOND_10UP :{$cond :[       {$gt :["$CALL_DURATION",10]}                                ,"$CALL_DURATION",0]}
         }}
         ,{$group:{
             _id: {
-                DATE : "$DATE"
+                  DATE : "$DATE"
                 , HOUR : "$HOUR"
                 , NETWORK_TYPE : "$NETWORK_TYPE"
                 , END_CODE: "$END_CODE"
@@ -56,9 +62,15 @@ var agg_3g = db.cep3g_join.aggregate([
                 //, VENDOR: "$VENDOR"
                 //, MODEL: "$MODEL"
             }
-            , HO_DISTINCT:{$addToSet:"$HO_DISTINCT"}
-            , HO_CALLED_COUNT:{$sum:"$HO"}
-            , HO_CALLED_SECOND:{$sum:"$HO_SECOND"}
+            //, HO_DISTINCT:{$addToSet:"$HO_DISTINCT"}
+            //, HO_CALLED_COUNT:{$sum:"$HO"}
+            //, HO_CALLED_SECOND:{$sum:"$HO_SECOND"}
+
+            , DISTINCT_CALLED_COUNT_0_3  :{$addToSet:"$DISTINCT_CALLED_COUNT_0_3"}
+            , DISTINCT_CALLED_COUNT_3_5  :{$addToSet:"$DISTINCT_CALLED_COUNT_3_5"}
+            , DISTINCT_CALLED_COUNT_5_7  :{$addToSet:"$DISTINCT_CALLED_COUNT_5_7"}
+            , DISTINCT_CALLED_COUNT_7_10 :{$addToSet:"$DISTINCT_CALLED_COUNT_7_10"}
+            , DISTINCT_CALLED_COUNT_10UP :{$addToSet:"$DISTINCT_CALLED_COUNT_10UP"}
 
             , SUM_CALLED_COUNT_0_3 : {$sum:"$SUM_CALLED_COUNT_0_3"}
             , SUM_CALLED_COUNT_3_5 : {$sum:"$SUM_CALLED_COUNT_0_3"}
@@ -89,10 +101,16 @@ var agg_3g = db.cep3g_join.aggregate([
             //, VENDOR        : "$_id.VENDOR"
             //, MODEL         : "$_id.MODEL"
 
-            , HO_DISTINCT       :1
-            , HO_CALLED_COUNT   :1
-            , HO_CALLED_SECOND  :1
-            , HO_CALLED_MINUTES :{$divide:["$HO_CALLED_SECOND",60]}
+            //, HO_DISTINCT       :1
+            //, HO_CALLED_COUNT   :1
+            //, HO_CALLED_SECOND  :1
+            //, HO_CALLED_MINUTES :{$divide:["$HO_CALLED_SECOND",60]}
+
+            , DISTINCT_CALLED_COUNT_0_3 :1
+            , DISTINCT_CALLED_COUNT_3_5 :1
+            , DISTINCT_CALLED_COUNT_5_7 :1
+            , DISTINCT_CALLED_COUNT_7_10:1
+            , DISTINCT_CALLED_COUNT_10UP:1
 
             , SUM_CALLED_COUNT_0_3 : 1
             , SUM_CALLED_COUNT_3_5 : 1
